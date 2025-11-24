38fed8f72e70abb28c027c5db592ebceac9d1dd5
“vannyakh” <vannakkm33@gmail.com>
2025-11-24 23:15:57 +0700

    chore: remove server files and directories to clean up the project structure

--- server/src/common/middlewares/not-found-handler.ts
+++
@@ -1,9 +0,0 @@
-import type { Request, Response } from 'express';
-import httpStatus from 'http-status';
-
-export const notFoundHandler = (_req: Request, res: Response): Response =>
-  res.status(httpStatus.NOT_FOUND).json({
-    success: false,
-    message: 'Resource not found',
-  });
-
--- server/src/modules/upload/index.ts
+++
@@ -1,5 +0,0 @@
-export { uploadRouter } from './upload.router';
-export * from './upload.controller';
-export * from './upload.model';
-export * from './upload.repository';
-
